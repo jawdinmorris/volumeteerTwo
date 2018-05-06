fdd68c6c48d9238439de908585a8d076af9b2f5f
jawdinmorris <jordanandrewmorris1@gmail.com>
2018-05-06 13:01:54 +1000

    Showing more detail on jobs show

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,11 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :users
+
+
+def full_address
+  self.street << ", " << self.city << ", " << self.country << ", " << self.postcode.to_s
+end
+
+
 end
