704d657975a281711a2de9e84081b284d4040b39
Jordan Morris <jordanandrewmorris1@gmail.com>
2018-05-10 13:17:21 +1000

    default from changed

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'postmaster@sandbox13631e88a78c4d9bace7bea5340d8de1.mailgun.org'
+  default from: 'jordanandrewmorris1@gmail.com'
   layout 'mailer'
 end
