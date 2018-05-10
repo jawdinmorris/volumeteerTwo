589a150e4c802ae9f756b938d7a188230e72817d
Jordan Morris <jordanandrewmorris1@gmail.com>
2018-05-10 13:37:14 +1000

    Titleize for those nice views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_and_belongs_to_many :skills
     has_and_belongs_to_many :users
     accepts_nested_attributes_for :skills, :reject_if => proc { |attributes| attributes['name'].blank?}
-
+    validates :phone, numericality: { only_integer: true }
+    validates :postcode, numericality: { only_integer: true }
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -18,15 +19,15 @@ class User < ApplicationRecord
 
 def full_name
   if self.has_role? :volunteer
-  "#{first_name} #{last_name}"
+  "#{first_name} #{last_name}".titleize
   elsif self.has_role? :charity
-  "#{business_name}"
+  "#{business_name}".titleize
   end
 end
 
 
 def full_address
-  "#{street}, #{city}, #{country}, #{postcode.to_s}"
+  "#{street}, #{city}, #{country}, #{postcode.to_s}".titleize
 end
 
 def assign_chosen_role
